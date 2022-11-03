81f1a81b9f0fc0e156e550328c6021c44db5c747
LDS196 <lihachevd87@gmail.com>
2022-11-03 18:43:57 +0400

    feat: add script in index in 4 folder

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,64 @@
+const fs = require('fs');
+const path = require('path');
 
+const copyFolder = path.join(__dirname, '/files-copy')
+const testFolder = path.join(__dirname, '/files');
+
+copyDir();
+
+function copyDir(){
+fs.access(copyFolder, fs.constants.F_OK, (err) =>{
+  if (err) {
+    createFolder(addFile)
+  } else {
+    clearFolder(addFile)
+  }
+})
+}
+
+function addFile (){
+  fs.readdir(testFolder, (err, files) =>{
+    if(err){
+      console.log(err);
+      return;
+    } else{
+      files.forEach(file =>{
+        fs.copyFile(path.join(testFolder, '/', file) ,path.join(copyFolder, '/', file), (err) =>{
+          if(err) {
+            console.log(err);
+            return;
+          } else {
+            console.log('Files added');
+          }
+        })
+      })
+    }
+  })
+};
+
+function clearFolder(callback){
+  fs.readdir(copyFolder, (err, files) =>{
+    if(err) {
+      console.log(err);
+      return;
+    } else {
+      files.forEach(file =>{
+        fs.unlink(path.join(copyFolder, '/', file), err => {
+          if(err) throw err; // не удалось удалить файл
+          console.log('Файл успешно удалён');
+       });
+      })
+    }
+  })
+  callback()
+}
+
+function createFolder(callback){
+  fs.mkdir(path.join(copyFolder), err => {
+    if(err){
+      console.log('Errorss');
+      return;
+    }
+  })
+  callback();
+}
