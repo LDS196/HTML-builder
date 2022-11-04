7af7d95ec4a6f26da86ff7ea329d95bf9fb93f08
LDS196 <lihachevd87@gmail.com>
2022-11-04 16:21:24 +0400

    feat: fix index.js in 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,21 +1,22 @@
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
 
 const output = fs.createWriteStream(path.join(__dirname, '/text.txt'));
-const { stdin, stdout } = process;
-
+const { stdin} = process;
 
 console.log('Enter your text!');
-
 stdin.on('data', chunk => {
   const myData = chunk.toString();
   if( myData.trim() == 'exit'){
-    process.exit()
+    console.log('Good bye');
+    process.exit();
   } else {
-    output.write(chunk)
+    output.write(chunk);
   }
 })
 
-process.on('exit', () => stdout.write('Удачи!'));
-
-
+process.on('SIGINT', () => {
+  console.log('Good bye');
+  process.exit();
+});
